2ff0fa5f8a457c3e1ea9b05eb7741adc27e5c5c9
Negrah <86295105+NEGRAH-S@users.noreply.github.com>
2023-07-06 22:17:39 +0530

    Update outline.js

--- Frontend/outline.js
+++ Frontend/outline.js
@@ -1,1 +1,3 @@
 // We will plan out things here and make new file accordingly 
+
+// need to fix how many different code we will use to navigate the website to multiple pages and servoce that we trynna include
